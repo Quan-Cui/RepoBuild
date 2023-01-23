7ba1e90bbc9ae06e1ad953fe8d224e4bf022a384
Quan Cui <quancuiwork@gmail.com>
2023-01-23 00:08:58 -0800

    ini

---
+++ R/create.repo.R
@@ -0,0 +1,46 @@
+#' create.repo
+#'
+#' @param proj_name R project folder name
+#' @param proj_path R project path, end without /
+#'
+#' @return a folder full with sub folders for R project developing use
+#' @export
+#'
+#' @examples create.repo(proj_name = "abc9368-2022", proj_path = "C:/Git")
+
+
+create.repo <- function(proj_name = "abc9368-2022",
+                        proj_path = "C:/QuanGit"){
+
+  proj_folder <- paste0(proj_path, "/", proj_name)
+  invisible(
+    if (!dir.exists(proj_folder)){
+      dir.create(proj_folder)
+      dir.create(paste0(proj_folder, "/", "R"))
+      dir.create(paste0(proj_folder, "/", "R", "/", "functions"))
+      dir.create(paste0(proj_folder, "/", "data"))
+      dir.create(paste0(proj_folder, "/", "input"))
+      dir.create(paste0(proj_folder, "/", "output"))
+      dir.create(paste0(proj_folder, "/", "output", "/", "figures"))
+      dir.create(paste0(proj_folder, "/", "output", "/", "tables"))
+      dir.create(paste0(proj_folder, "/", "instructions"))
+      file.create(paste0(proj_folder, "/", ".gitignore"))
+      file.create(paste0(proj_folder, "/", "README.md"))
+      message(paste("Done. Created subfolders for a new folder", proj_folder))
+    } else{
+      dir.create(paste0(proj_folder, "/", "R"))
+      dir.create(paste0(proj_folder, "/", "R", "/", "functions"))
+      dir.create(paste0(proj_folder, "/", "data"))
+      dir.create(paste0(proj_folder, "/", "input"))
+      dir.create(paste0(proj_folder, "/", "output"))
+      dir.create(paste0(proj_folder, "/", "output", "/", "figures"))
+      dir.create(paste0(proj_folder, "/", "output", "/", "tables"))
+      dir.create(paste0(proj_folder, "/", "instructions"))
+      file.create(paste0(proj_folder, "/", ".gitignore"))
+      file.create(paste0(proj_folder, "/", "README.md"))
+      message(paste("Done. Created subfolders for an exisiting folder", proj_folder))
+    }
+  )
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # EngRepo
+
+<!-- badges: start -->
+[![R-CMD-check](https://github.com/Quan-Cui/EngRepo/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/Quan-Cui/EngRepo/actions/workflows/R-CMD-check.yaml)
+<!-- badges: end -->
+
+
+> To install this package
+> devtools::install_github("Quan-Cui/EngRepo")
