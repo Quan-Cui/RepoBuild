39236100b009db60a27ea942794f1fb6fa208995
Quan-Cui <quancuiwork@gmail.com>
2025-08-18 11:11:19 -0600

    add boreal function

--- R/RepoBuild.R
+++ R/RepoBuild.R
@@ -28,3 +28,39 @@ RepoBuild <- function(proj_folder) {
     }
   )
 }
+
+
+
+#' RepoBuild Boreal
+#'
+#' Create a folder structure within your project path.Designed for Boreal
+#'
+#' @param proj_folder your project folder
+#'
+#' @returns Structured folder
+#' @importFrom usethis create_project
+#' @export
+#'
+#' @examples
+#'  \dontrun{
+#' RepoBuild_Boreal("C:/myproject1")
+#' }
+RepoBuild_Boreal <- function(proj_folder){
+  invisible(
+    if (!dir.exists(proj_folder)) {
+      dir.create(proj_folder)
+      usethis::create_project(proj_folder)
+      create_subdirs_Boral(proj_folder)
+      update_gitignore(proj_folder)
+      message(paste("Done. Created subfolders for a new folder", proj_folder))
+    } else {
+      usethis::create_project(proj_folder)
+      create_subdirs_Boral(proj_folder)
+      update_gitignore(proj_folder)
+      message(paste("Done. Created subfolders for an existing folder", proj_folder))
+    }
+  )
+}
+
+
+
